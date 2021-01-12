b70cf134376d5d42d0f51eea38775ab4191aa820
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2021-01-12 21:41:15 +0100

    Provide basic functionality
    Add Edit menu with rotations, open and save options, implement modification history

--- gui.py
+++ gui.py
@@ -49,16 +49,16 @@ class ScrollableList(ttk.Frame):
         self.__elements_var = StringVar(value=self.__elements)
         self.__callback_function = None
 
-        list_box = Listbox(self, listvariable=self.__elements_var)
-        list_box.grid(row=0, column=0, sticky=(N, S, W, E))
+        self.__list_box = Listbox(self, listvariable=self.__elements_var)
+        self.__list_box.grid(row=0, column=0, sticky=(N, S, W, E))
 
-        v_scroll = ttk.Scrollbar(self, orient=VERTICAL, command=list_box.yview)
+        v_scroll = ttk.Scrollbar(self, orient=VERTICAL, command=self.__list_box.yview)
         v_scroll.grid(row=0, column=1, sticky=(N, S))
 
-        h_scroll = ttk.Scrollbar(self, orient=HORIZONTAL, command=list_box.xview)
+        h_scroll = ttk.Scrollbar(self, orient=HORIZONTAL, command=self.__list_box.xview)
         h_scroll.grid(row=1, column=0, sticky=(W, E))
 
-        list_box.bind("<<ListboxSelect>>", self.__callback)
+        self.__list_box.bind("<<ListboxSelect>>", self.__callback)
 
     @property
     def list(self):
@@ -68,6 +68,7 @@ class ScrollableList(ttk.Frame):
     def list(self, new_list):
         self.__elements = new_list
         self.__elements_var.set(self.__elements)
+        self.clear_selection()
 
     @property
     def callback_function(self):
@@ -83,6 +84,12 @@ class ScrollableList(ttk.Frame):
             index = selection[0]
             self.__callback_function(index)
 
+    def clear_selection(self):
+        self.__list_box.selection_clear(0, END)
+
+    def get_selection(self):
+        return self.__list_box.curselection()
+
 
 class MainWindow:
     def __init__(self, root: Tk):
@@ -97,11 +104,13 @@ class MainWindow:
         main_window.columnconfigure(1, weight=2)
         main_window.grid(row=0, column=0, sticky=(N, S, W, E))
 
-        left_image_window = ScrollableImage(main_window, image=self.__image_manager.image)
-        left_image_window.grid(row=0, column=0, sticky=(N, S, W, E))
+#       Create scrollable images
 
-        right_image_window = ScrollableImage(main_window, image=self.__image_manager.small_image)
-        right_image_window.grid(row=0, column=1, sticky=(N, S, W, E))
+        self.__left_image_window = ScrollableImage(main_window, image=self.__image_manager.image)
+        self.__left_image_window.grid(row=0, column=0, sticky=(N, S, W, E))
+
+        self.__right_image_window = ScrollableImage(main_window, image=self.__image_manager.manipulated_image)
+        self.__right_image_window.grid(row=0, column=1, sticky=(N, S, W, E))
 
 #       Create menu bar
 
@@ -110,19 +119,44 @@ class MainWindow:
         menu_bar = Menu(root)
 
         file_menu = Menu(menu_bar)
-        file_menu.add_command(label='Open')
-        file_menu.add_command(label='Save')
-        file_menu.add_command(label="Save as...")
+        file_menu.add_command(label='Open', command=self.open_file)
+        file_menu.add_command(label="Save", command=self.save_image_as)
+        menu_bar.add_cascade(menu=file_menu, label='File')
+
+        edit_menu = Menu(menu_bar)
+        edit_menu.add_command(label='Rotate 90° ↷', command=self.rotate_by_90)
+        edit_menu.add_command(label='Rotate 180° ↷', command=self.rotate_by_180)
+        edit_menu.add_command(label='Rotate -90° ↶', command=self.rotate_by_270)
+        menu_bar.add_cascade(menu=edit_menu, label='Edit')
 
         simple_filters_menu = Menu(menu_bar)
         simple_filters_menu.add_command(label='Canny')
-
-        menu_bar.add_cascade(menu=file_menu, label='File')
         menu_bar.add_cascade(menu=simple_filters_menu, label='Simple filters')
 
         root['menu'] = menu_bar
 
+#       Create status bar with scale
+
+        self.__scale_var = StringVar()
+        self.__scale_val = 100
+        self.__scale_var.set("100%")
+
+        bottom_panel = ttk.Frame(main_window)
+        bottom_panel.columnconfigure(0, weight=1)
+        bottom_panel.grid(row=1, column=0, sticky=(W, E), columnspan=3)
+
+        self.__status_bar = ttk.Label(bottom_panel, text="status:", anchor="w")
+        self.__status_bar.grid(row=0, column=0, sticky=(E, W))
+
+        scale_label = ttk.Label(bottom_panel, textvariable=self.__scale_var)
+        scale_label.grid(row=0, column=1)
+
+        scale = ttk.Scale(bottom_panel, from_=1, to=200, orient=HORIZONTAL, command=self.scale_image)
+        scale.set(100)
+        scale.grid(row=0, column=2)
+
 #       Create right panel
+
         main_window.columnconfigure(2, weight=1)
         right_panel = ttk.Frame(main_window)
         right_panel.grid(row=0, column=2, sticky=(N, S, W, E))
@@ -155,14 +189,73 @@ class MainWindow:
         queue_separator.grid(row=1, column=0, sticky=(W, E))
         queue_panel.columnconfigure(0, weight=1)
 
-        queue = ScrollableList(queue_panel)
-        queue.callback_function = print
-        queue.list = ["aaaaa", "bbbbbb"]
-        queue.grid(row=2, column=0, sticky=(N, S, W, E))
+        self.__queue = ScrollableList(queue_panel)
+        self.__queue.callback_function = self.show_prev_image
+        self.__queue.list = self.__image_manager.prev_commands
+        self.__queue.grid(row=2, column=0, sticky=(N, S, W, E))
+
+    def open_file(self):
+        filename = fd.askopenfilename(title="Select image", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png")))
+        if filename != "":
+            answer = messagebox.askyesno(title="Open Image", message="Do you want to open new Image? "
+                                                                     "All unsaved work will be lost!")
+            if answer is True:
+                self.__image_manager.open_image_from_path(filename)
+                self.__left_image_window.image = self.__image_manager.image
+                self.__right_image_window.image = self.__image_manager.manipulated_image
+                self.__queue.list = self.__image_manager.prev_commands
+                self.__queue.clear_selection()
+
+    def scale_image(self, scale):
+        scale_temp_int = int(getdouble(scale))
+        if scale_temp_int != self.__scale_val:
+            self.__scale_val = scale_temp_int
+            self.__scale_var.set(str(self.__scale_val) + "%")
+            self.__image_manager.scale = scale_temp_int
+            self.__left_image_window.image = self.__image_manager.image
+            self.__right_image_window.image = self.__image_manager.manipulated_image
+            self.__queue.clear_selection()
+
+    def rotate_by_90(self):
+        self.__image_manager.rotate_by_90()
+        self.__right_image_window.image=self.__image_manager.manipulated_image
+        self.__queue.list = self.__image_manager.prev_commands
+
+    def rotate_by_180(self):
+        self.__image_manager.rotate_by_180()
+        self.__right_image_window.image = self.__image_manager.manipulated_image
+        self.__queue.list = self.__image_manager.prev_commands
+
+    def rotate_by_270(self):
+        self.__image_manager.rotate_by_270()
+        self.__right_image_window.image = self.__image_manager.manipulated_image
+        self.__queue.list = self.__image_manager.prev_commands
+
+    def show_prev_image(self, i):
+        self.__right_image_window.image=self.__image_manager.get_prev_image(i)
+
+    def save_image_as(self):
+        filename = fd.asksaveasfile(title="Save as...", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png")), defaultextension=".jpg")
+
+        if filename is not None:
+            filename = filename.name
+            if re.match(r".*?((\.jpg)|(\.png))", filename) is None:
+                print(re.match(r".*?((\.jpg)|(\.png))", filename))
+                messagebox.showerror(title="Incorrect name", message=f"Path:{filename} is incorrect\nFilename must end "
+                                                                     f"with .png or .jpg")
+                return
+            if self.__scale_val != 100:
+                scale = messagebox.askyesno(title="Scale Image?", message="Do you want to save image with default "
+                                                                           "scale? If not you may expect decrease of"
+                                                                           " quality")
+                self.__image_manager.save_image(filename, scale, self.__queue.get_selection())
+            else:
+                self.__image_manager.save_image(filename, False, self.__queue.get_selection())
 
 
 if __name__ == '__main__':
     root = Tk()
     app = MainWindow(root)
+    root.minsize(1000, 500)
     root.geometry("1000x500")
     root.mainloop()
--- image_manager.py
+++ image_manager.py
@@ -5,19 +5,97 @@ from PIL import ImageTk
 
 class ImageManager:
     def __init__(self):
-        self.__image = cv2.imread("C:/Users/HP/Pictures/profile.png")
-        self.__small_image = cv2.imread("C:/Users/HP/Pictures/azunya.png")
+        #  image in BGR
+        self.__image = cv2.imread("C:/Users/HP/Pictures/azunya.png")
+        # manipulated image by our functions
+        self.__manipulated_image = cv2.imread("C:/Users/HP/Pictures/azunya.png")
+        self.__is_grayscale = False
+
+        # stack of images
+        self.__prev_images = [(self.__image, False)]
+        # stack of strings each containing description of used command
+        self.__prev_commands = ["Initial state"]
+        # scale of images showed to user. Note: images are stored in original size.
+        self.__scale = 100
 
     @property
     def image(self):
-        image = cv2.cvtColor(self.__image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)
-        image = ImageTk.PhotoImage(image)
-        return image
+        return self.image_to_tk(self.__image, False)
 
     @property
-    def small_image(self):
-        image = cv2.cvtColor(self.__small_image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)
-        image = ImageTk.PhotoImage(image)
-        return image
+    def manipulated_image(self):
+        return self.image_to_tk(self.__manipulated_image, self.__is_grayscale)
+
+    @property
+    def scale(self):
+        return self.__scale
+
+    @scale.setter
+    def scale(self, new_scale):
+        self.__scale = new_scale
+
+    @property
+    def prev_commands(self):
+        return self.__prev_commands
+
+    def __scale_image(self, image):
+        if self.__scale != 100:
+            width = int(image.shape[1] * self.__scale / 100)
+            height = int(image.shape[0] * self.__scale / 100)
+            return cv2.resize(image, (width, height))
+        else:
+            return image
+
+    def image_to_tk(self, cv_image, is_grayscale):
+        if not is_grayscale:
+            tk_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            tk_image = self.__scale_image(tk_image)
+        else:
+            tk_image = self.__scale_image(cv_image)
+        tk_image = Image.fromarray(tk_image)
+        tk_image = ImageTk.PhotoImage(tk_image)
+        return tk_image
+
+    def get_prev_image(self, i):
+        image, gray = self.__prev_images[i]
+        return self.image_to_tk(image, gray)
+
+    def open_image_from_path(self, path):
+        self.__image = cv2.imread(path, cv2.IMREAD_COLOR)
+        self.__manipulated_image = cv2.imread(path, cv2.IMREAD_COLOR)
+        self.__prev_images = [(self.__image, False)]
+        self.__prev_commands = ["Initial state"]
+        self.__is_grayscale = False
+
+    def rotate_by_90(self):
+        self.__prev_commands.append("Rotate by 90 degrees")
+        self.__manipulated_image = cv2.rotate(self.__manipulated_image, cv2.ROTATE_90_CLOCKWISE)
+        self.__prev_images.append((self.__manipulated_image, self.__is_grayscale))
+
+    def rotate_by_180(self):
+        self.__prev_commands.append("Rotate by 180 degrees")
+        self.__manipulated_image = cv2.rotate(self.__manipulated_image, cv2.ROTATE_180)
+        self.__prev_images.append((self.__manipulated_image, self.__is_grayscale))
+
+    def rotate_by_270(self):
+        self.__prev_commands.append("Rotate by -90 degrees")
+        self.__manipulated_image = cv2.rotate(self.__manipulated_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        self.__prev_images.append((self.__manipulated_image, self.__is_grayscale))
+
+    def save_image(self, path: str, scale: bool, number=None):
+        if number == ():
+            if scale:
+                print("nn s")
+                cv2.imwrite(path, self.__scale_image(self.__manipulated_image))
+            else:
+                print("nn ns")
+
+                cv2.imwrite(path, self.__manipulated_image)
+
+        else:
+            if scale:
+                cv2.imwrite(path, self.__prev_images[number][0])
+                print("n s")
+            else:
+                cv2.imwrite(path, self.__prev_images[number][0])
+                print("nn s")
